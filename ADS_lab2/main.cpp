6c3ecb306bc920ae3945fc29dbca9c401e2066a3
helga-helga <olyalavyagina@gmail.com>
2015-09-03 13:06:31 +0300

    Initial commit

---
+++ ADS_lab2/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: olya
+ *
+ * Created on September 3, 2015, 1:04 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
---
+++ ADS_lab2/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_ARTIFACT_NAME_Debug=ads_lab2
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/ads_lab2
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=adslab2.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/adslab2.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=ads_lab2
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/ads_lab2
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=adslab2.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/adslab2.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
