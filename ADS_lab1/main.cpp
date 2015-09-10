68e8a54751e64a18f25b22046e091a4472a0083a
helga-helga <olyalavyagina@gmail.com>
2015-09-10 15:33:21 +0300

    Menu added

--- ADS_lab1/main.cpp
+++ ADS_lab1/main.cpp
@@ -14,14 +14,50 @@ using namespace std;
  * 
  */
 int main(int argc, char** argv) {
-    int power;
-    float number;
-    cout<<"Enter a valid non-zero number ";
-    cin>>number;
-    cout<<"Enter a positive integral number ";
-    cin>>power;
-    cout<<number<<"^"<<power<<"="<<exponentiation(power, number);
-
+    int variant;
+    
+    while (true){
+        
+        cout<<"Choose a variant"<<endl;
+        cout<<"1. exponentiation\n"
+            <<"2. finding the greatest common divisor\n"
+            <<"3. calculation of Fibonacci number\n"
+            <<"4. tower of Hanoi\n"
+            <<"5. exit\n";
+        cin>>variant;
+        
+        switch(variant){
+            case 1:
+                int power;
+                float number;
+                cout<<"Enter a valid non-zero number ";
+                cin>>number;
+                cout<<"Enter a positive integral number ";
+                cin>>power;
+                cout<<number<<"^"<<power<<"="<<exponentiation(power, number)<<endl;
+                break;
+            case 2:
+                int m, n;
+                cout<<"Enter two numbers";
+                cin>>n>>m;
+                cout<<"The greatest common divisor of "<<n<<" and "<<m<<" is "<<gcd(m,n)<<endl;
+                break;
+            case 3:
+                cout<<"Fibonacci number";
+                break;
+            case 4:
+                cout<<"The tower of Hanoi";
+                break;
+            case 5:
+                cout<<"Exiting..."<<endl;
+                exit(EXIT_SUCCESS);
+                break;
+            default:
+                cerr<<"You've chosen the wrong variant";
+                exit(EXIT_FAILURE);
+        }
+                   
+    }
     return 0;
 }
 
