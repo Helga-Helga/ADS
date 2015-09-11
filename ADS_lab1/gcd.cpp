d1406682fafee4e519ca5475dda15539e3769631
helga-helga <olyalavyagina@gmail.com>
2015-09-11 09:59:29 +0300

    Using 'else if' instead of 'if'

--- ADS_lab1/gcd.cpp
+++ ADS_lab1/gcd.cpp
@@ -1,11 +1,11 @@
 #include "Header.h"
 
-int gcd(int m, int n){
-    if ((n==0)&&(m==0)){
+int gcd(int m, int n) {
+    if ((n==0)&&(m==0)) {
         throw invalid_argument("Invalid syntax.");
     }         
             
-    if (n==0) {
+    else if (n==0) {
         return (m < 0)? -m : m;
     }
     return gcd(n, m%n);
