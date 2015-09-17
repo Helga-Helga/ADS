9a04029358ab9c8e62bbce5ec37ac3aa8d037445
helga-helga <olyalavyagina@gmail.com>
2015-09-17 18:49:05 +0300

    Added function to check if the list was sorted

--- ADS_lab2/main.py
+++ ADS_lab2/main.py
@@ -8,6 +8,17 @@ from quick_sort import quick_sort
 s = [randint(0, 5) for i in range(int(argv[1]))]
 print(s)
 
+print("Bubble_sort: ")
 bubble_sort(s)
-quick_sort(s)
+print "Quick_sort: "
+s = quick_sort(s)
+print(s)
+
+def check(a, b):
+    if a > b:
+        raise Exception('Not sorted: ' + str(a) + ' should not be greater than ' + str(b))
+    return b
+
+reduce(check, s)
+
 
