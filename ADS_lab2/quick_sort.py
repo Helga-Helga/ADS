10c5eb3d3810850692e60e6c2bae9bd207745bbb
helga-helga <olyalavyagina@gmail.com>
2015-09-17 16:20:49 +0300

    Added function quick_sort()

--- ADS_lab2/quick_sort.py
+++ ADS_lab2/quick_sort.py
@@ -1,1 +1,11 @@
+from random import random
+
 __author__ = 'olya'
+
+def quick_sort(samples):
+    if len(samples) > 1:
+        pivot = random.choice(samples)
+    less = [x for x in samples if x < pivot]
+    greater = [x for x in samples if x > pivot]
+    equal = [x for x in samples if x == pivot]
+    return quick_sort(less) + equal + quick_sort(greater)
