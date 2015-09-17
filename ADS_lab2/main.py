76b051bb9530855bf2ce3e93b388a7237ed67073
helga-helga <olyalavyagina@gmail.com>
2015-09-17 17:44:40 +0300

    Created list of numbers

--- ADS_lab2/main.py
+++ ADS_lab2/main.py
@@ -1,8 +1,13 @@
 __author__ = 'olya'
 
+from sys import argv
 from random import randint
-samples = [
-    randint(0, 10) in range(100),
-    randint(0, 10) in range(1000),
-    randint(0, 10) in range(10000),
-]
+from bubble_sort import bubble_sort
+from quick_sort import quick_sort
+
+s = [randint(0, 5) for i in range(int(argv[1]))]
+print(s)
+
+bubble_sort(s)
+quick_sort(s)
+
