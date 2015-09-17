59b3d39a1df9dcb887ae85e00f75d1fbc1b0d3bd
helga-helga <olyalavyagina@gmail.com>
2015-09-17 18:59:15 +0300

    Added a condition if len(sample) < 2

--- ADS_lab2/quick_sort.py
+++ ADS_lab2/quick_sort.py
@@ -1,10 +1,11 @@
-from random import random
-
 __author__ = 'olya'
 
+from random import randint
+
 def quick_sort(sample):
-    if len(sample) > 1:
-        pivot = random.choice(sample)
+    if len(sample) < 2:
+        return sample
+    pivot = sample[randint(0, len(sample)-1)]
     less = [x for x in sample if x < pivot]
     greater = [x for x in sample if x > pivot]
     equal = [x for x in sample if x == pivot]
