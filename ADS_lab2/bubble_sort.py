33daf761344f6400f788a0a42472b2442b49d442
helga-helga <olyalavyagina@gmail.com>
2015-09-17 13:25:30 +0300

    Added function bubble_sort()

--- ADS_lab2/bubble_sort.py
+++ ADS_lab2/bubble_sort.py
@@ -1,1 +1,7 @@
 __author__ = 'olya'
+
+def bubble_sort(samples):
+    for i in range(len(samples), 0, -1):
+        for j in range(0, len(samples) - 1):
+            if samples[j] > samples[j+1]:
+                samples[j], samples[j+1] = samples[j+1], samples[j]
