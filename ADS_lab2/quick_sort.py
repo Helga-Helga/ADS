2d410692ddf062ad63684d1effe41efcb06e154a
helga-helga <olyalavyagina@gmail.com>
2015-09-17 17:43:07 +0300

    Created list of numbers

--- ADS_lab2/quick_sort.py
+++ ADS_lab2/quick_sort.py
@@ -2,10 +2,10 @@ from random import random
 
 __author__ = 'olya'
 
-def quick_sort(samples):
-    if len(samples) > 1:
-        pivot = random.choice(samples)
-    less = [x for x in samples if x < pivot]
-    greater = [x for x in samples if x > pivot]
-    equal = [x for x in samples if x == pivot]
+def quick_sort(sample):
+    if len(sample) > 1:
+        pivot = random.choice(sample)
+    less = [x for x in sample if x < pivot]
+    greater = [x for x in sample if x > pivot]
+    equal = [x for x in sample if x == pivot]
     return quick_sort(less) + equal + quick_sort(greater)
