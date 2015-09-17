69ca2b0dc524cdd80dddc455f492e98f88b39b23
helga-helga <olyalavyagina@gmail.com>
2015-09-17 19:23:42 +0300

    Changed algorythm

--- ADS_lab2/quick_sort.py
+++ ADS_lab2/quick_sort.py
@@ -2,11 +2,18 @@ __author__ = 'olya'
 
 from random import randint
 
-def quick_sort(sample):
-    if len(sample) < 2:
-        return sample
-    pivot = sample[randint(0, len(sample)-1)]
-    less = [x for x in sample if x < pivot]
-    greater = [x for x in sample if x > pivot]
-    equal = [x for x in sample if x == pivot]
-    return quick_sort(less) + equal + quick_sort(greater)
+def quick_sort(sample, p, r):
+    if p < r:
+        q = partition(sample, p, r)
+        quick_sort(sample, p, q - 1)
+        quick_sort(sample, q+1, r)
+
+def partition(sample, p, r):
+    x = sample[r]
+    i = p - 1
+    for j in range(p, r-1):
+        if sample[j] <= x:
+            i += 1
+            sample[i], sample[j] = sample[j], sample[i]
+    sample[i+1], sample[r] = sample[r], sample[i+1]
+    return i+1
