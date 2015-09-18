433bc7c99702c7aeb90550d2cf1336c9ccb1ebdc
helga-helga <olyalavyagina@gmail.com>
2015-09-18 08:20:12 +0300

    Output only real_quick_sorted list

--- ADS_lab2/main.py
+++ ADS_lab2/main.py
@@ -3,16 +3,14 @@ __author__ = 'olya'
 from sys import argv
 from random import randint
 from bubble_sort import bubble_sort
-from quick_sort import quick_sort
+from quick_sort import quick_sort, real_quick_sort
 
 s = [randint(0, 5) for i in range(int(argv[1]))]
 print(s)
 
-print("Bubble_sort: ")
-bubble_sort(s)
-print "Quick_sort: "
-s = quick_sort(s)
-print(s)
+print "Real quick sort: "
+real_quick_sort(s, s[0], len(s)-1)
+print (s)
 
 def check(a, b):
     if a > b:
