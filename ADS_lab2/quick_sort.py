61b887fb809dbef70ae9186281ab2d4a47632f96
helga-helga <olyalavyagina@gmail.com>
2015-09-18 15:09:23 +0300

    Added function get_corners() that doesn't compare the same elements

--- ADS_lab2/quick_sort.py
+++ ADS_lab2/quick_sort.py
@@ -14,15 +14,24 @@ def quick_sort(sample):
 def real_quick_sort(sample, p, r):
     if p < r:
         q = partition(sample, p, r)
-        real_quick_sort(sample, p, q - 1)
-        real_quick_sort(sample, q + 1, r)
+        i, j = get_corners(sample, p, r, q)
+        real_quick_sort(sample, p, i)
+        real_quick_sort(sample, j, r)
 
 def partition(sample, p, r):
     x = sample[r]
-    i = p - 1
+    i = p -1
     for j in range(p, r):
-        if sample[j] <= x:
+        if sample[j] < x:
             i += 1
             sample[i], sample[j] = sample[j], sample[i]
     sample[i+1], sample[r] = sample[r], sample[i+1]
     return i+1
+
+def get_corners(sample, p, r, q):
+    i, j = q - 1, q + 1
+    while sample[i] == sample[q] and i > p:
+        i -= 1
+    while sample[j] == sample[q] and j < r:
+        j += 1
+    return i, j
