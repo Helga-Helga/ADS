1d71260544530a0e8d5303f6420053aef904e5a3
helga-helga <olyalavyagina@gmail.com>
2015-09-18 15:11:07 +0300

    Added menu

--- ADS_lab2/main.py
+++ ADS_lab2/main.py
@@ -2,21 +2,40 @@ __author__ = 'olya'
 
 from sys import argv
 from random import randint
+from copy import copy
 from bubble_sort import bubble_sort
 from quick_sort import quick_sort, real_quick_sort
 
 s = [randint(0, 5) for i in range(int(argv[1]))]
 print(s)
 
-print "Real quick sort: "
-real_quick_sort(s, s[0], len(s)-1)
-print (s)
-
 def check(a, b):
     if a > b:
         raise Exception('Not sorted: ' + str(a) + ' should not be greater than ' + str(b))
     return b
 
-reduce(check, s)
+answer = int(argv[2])
+if answer == 1:
+    sample = copy(s)
+    print "Bubble sort: "
+    bubble_sort(sample)
+    reduce(check, sample)
+elif answer == 2:
+    sample = copy(s)
+    sample = quick_sort(sample)
+    print "Quick sort: "
+    print (sample)
+    reduce(check, sample)
+elif answer ==3:
+    sample = copy(s)
+    print "Real quick sort: "
+    real_quick_sort(sample, 0, len(sample)-1)
+    print (sample)
+    reduce(check, sample)
+else:
+    raise Exception('Bad input')
+
+
+
 
 
