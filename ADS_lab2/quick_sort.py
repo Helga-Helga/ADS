fc7a8e8e8931249d3410428b185259e5a589fe01
helga-helga <olyalavyagina@gmail.com>
2015-09-18 08:18:29 +0300

    Used two algorithms

--- ADS_lab2/quick_sort.py
+++ ADS_lab2/quick_sort.py
@@ -2,16 +2,25 @@ __author__ = 'olya'
 
 from random import randint
 
-def quick_sort(sample, p, r):
+def quick_sort(sample):
+    if len(sample) < 2:
+        return sample
+    pivot = sample[randint(0, len(sample)-1)]
+    less = [x for x in sample if x < pivot]
+    greater = [x for x in sample if x > pivot]
+    equal = [x for x in sample if x == pivot]
+    return quick_sort(less) + equal + quick_sort(greater)
+
+def real_quick_sort(sample, p, r):
     if p < r:
         q = partition(sample, p, r)
-        quick_sort(sample, p, q - 1)
-        quick_sort(sample, q+1, r)
+        real_quick_sort(sample, p, q - 1)
+        real_quick_sort(sample, q + 1, r)
 
 def partition(sample, p, r):
     x = sample[r]
     i = p - 1
-    for j in range(p, r-1):
+    for j in range(p, r):
         if sample[j] <= x:
             i += 1
             sample[i], sample[j] = sample[j], sample[i]
