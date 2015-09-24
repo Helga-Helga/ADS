de69518880dc8103c381771d7731041a50d22b88
helga-helga <olyalavyagina@gmail.com>
2015-09-24 16:18:13 +0300

    Added counters for exchanges and comparisons for first algorithm

--- ADS_lab2/quick_sort.py
+++ ADS_lab2/quick_sort.py
@@ -2,14 +2,34 @@ __author__ = 'olya'
 
 from random import randint
 
-def quick_sort(sample):
-    if len(sample) < 2:
-        return sample
-    pivot = sample[randint(0, len(sample)-1)]
-    less = [x for x in sample if x < pivot]
-    greater = [x for x in sample if x > pivot]
-    equal = [x for x in sample if x == pivot]
-    return quick_sort(less) + equal + quick_sort(greater)
+
+def quick_sort(s):
+    info = {
+        'comparisons': 0
+    }
+
+    def _quick_sort(sample):
+        less = []
+        greater = []
+        equal = []
+        if len(sample) < 2:
+            return sample
+        pivot = sample[randint(0, len(sample) - 1)]
+        for x in sample:
+            if x < pivot:
+                info['comparisons'] += 1
+                less.append(x)
+            elif x > pivot:
+                info['comparisons'] += 1
+                greater.append(x)
+            else:
+                info['comparisons'] += 1
+                equal.append(x)
+        return _quick_sort(less) + equal + _quick_sort(greater)
+
+    info['sorted'] = _quick_sort(s)
+    return info['comparisons'], info['sorted']
+
 
 def real_quick_sort(sample, p, r):
     if p < r:
@@ -18,15 +38,17 @@ def real_quick_sort(sample, p, r):
         real_quick_sort(sample, p, i)
         real_quick_sort(sample, j, r)
 
+
 def partition(sample, p, r):
     x = sample[r]
-    i = p -1
+    i = p - 1
     for j in range(p, r):
         if sample[j] < x:
             i += 1
             sample[i], sample[j] = sample[j], sample[i]
-    sample[i+1], sample[r] = sample[r], sample[i+1]
-    return i+1
+    sample[i + 1], sample[r] = sample[r], sample[i + 1]
+    return i + 1
+
 
 def get_corners(sample, p, r, q):
     i, j = q - 1, q + 1
