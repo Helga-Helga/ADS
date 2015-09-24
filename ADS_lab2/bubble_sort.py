557201992ba36630b83af8570e3a897ab698f3f7
helga-helga <olyalavyagina@gmail.com>
2015-09-24 16:17:04 +0300

    Added counters for exchanges and comparisons

--- ADS_lab2/bubble_sort.py
+++ ADS_lab2/bubble_sort.py
@@ -1,8 +1,14 @@
 __author__ = 'olya'
 
+
 def bubble_sort(sample):
+    m = 0
+    k = 0
     for i in range(1, len(sample)):
-        for j in range(len(sample)-i):
-            if sample[j] > sample[j+1]:
-                sample[j], sample[j+1] = sample[j+1], sample[j]
-    print sample
+        for j in range(len(sample) - i):
+            k += 1
+            if sample[j] > sample[j + 1]:
+                sample[j], sample[j + 1] = sample[j + 1], sample[j]
+                m += 1
+    print 'Comparisons: ', k
+    print 'Exchanges: ', m
