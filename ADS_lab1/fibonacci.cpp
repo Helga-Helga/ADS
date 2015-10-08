6a20d4b6429571c039f4ef8b1265a08caebf9523
helga-helga <olyalavyagina@gmail.com>
2015-10-08 09:19:31 +0300

    Added condition to n == 1

--- ADS_lab1/fibonacci.cpp
+++ ADS_lab1/fibonacci.cpp
@@ -5,12 +5,13 @@ int fibonacci(unsigned int n){
     if (known[n] != 0) return known[n];
     int t = n;
     if (n < 1) return 0;
-    if (n > 1) t=fibonacci(n-1)+fibonacci(n-2);
+    else if (n==1) return 1;
+    else if (n > 1) t=fibonacci(n-1)+fibonacci(n-2);
     return known[n]=t;
 }
 
 int F(unsigned int n) {
     if (n < 1) return 0;
-    if (n == 1) return 1;
+    else if (n == 1) return 1;
     return F(n-1) + F(n-2);
 }
