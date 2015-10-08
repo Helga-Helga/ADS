5d23d13ab2ae41a2b8de2408d2d27d656cb47e11
helga-helga <olyalavyagina@gmail.com>
2015-10-08 09:35:13 +0300

    Added file DbLinkedList.c

--- ADS_lab3/nbproject/Makefile-Release.mk
+++ ADS_lab3/nbproject/Makefile-Release.mk
@@ -35,6 +35,7 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
+	${OBJECTDIR}/DbLinkedList.o \
 	${OBJECTDIR}/main.o
 
 
@@ -61,6 +62,11 @@ LDLIBSOPTIONS=
 ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/ads_lab3: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.c} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/ads_lab3 ${OBJECTFILES} ${LDLIBSOPTIONS}
+
+${OBJECTDIR}/DbLinkedList.o: DbLinkedList.c 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.c) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/DbLinkedList.o DbLinkedList.c
 
 ${OBJECTDIR}/main.o: main.c 
 	${MKDIR} -p ${OBJECTDIR}
