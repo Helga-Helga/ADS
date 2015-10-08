e8b10baa139785f4df95f29def064c71a459a262
helga-helga <olyalavyagina@gmail.com>
2015-10-08 15:24:43 +0300

    Added header file

---
+++ ADS_lab3/element.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   element.h
+ * Author: olya
+ *
+ * Created on October 8, 2015, 3:23 PM
+ */
+
+#ifndef ELEMENT_H
+#define	ELEMENT_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* ELEMENT_H */
+
