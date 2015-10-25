8c870a95fa6867c9f9e64f5bf6531b5f7bda05c4
helga-helga <olyalavyagina@gmail.com>
2015-10-25 14:12:03 +0200

    Added deletion of lists

--- ADS_lab3/tests/listSize_test.c
+++ ADS_lab3/tests/listSize_test.c
@@ -7,11 +7,13 @@
 void testListSize() {
 	struct Element* list = insertElement(createElement('a'), createElement('b'), 1);
 	CU_ASSERT_EQUAL(listSize(list), 2);
+	deleteList(list);
 }
 
 void testSingleElementSize() {
 	struct Element* elem = createElement('a');
 	CU_ASSERT_EQUAL(listSize(elem), 1);
+	deleteList(elem);
 }
 
 void testNULLSize() {
