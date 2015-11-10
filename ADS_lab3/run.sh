9bfd0df5f479df695bfebaf88d824d0de59137dd
helga-helga <olyalavyagina@gmail.com>
2015-11-10 17:02:03 +0200

    Fixed error: rewriting out files before cycle

--- ADS_lab3/run.sh
+++ ADS_lab3/run.sh
@@ -1,7 +1,8 @@
+>array_out.txt
+>list_out.txt
 cat strings.txt | while read string
 do
-	>array_out.txt
-	>list_out.txt
+	echo "Processing '$string'"
 	./lab3 array "$string" >> array_out.txt
 	./lab3 list "$string" >> list_out.txt
 done
