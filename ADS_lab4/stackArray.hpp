56a64e8e657f3143cd1cca84d7a0373893c989a2
helga-helga <olyalavyagina@gmail.com>
2015-11-29 16:04:16 +0200

    Implemented stack with array

--- ADS_lab4/stackArray.hpp
+++ ADS_lab4/stackArray.hpp
@@ -2,5 +2,23 @@
 #include "stack.hpp"
 
 template <class T> class StackArray : public Stack<T> {
-
+private:
+	T *array;
+	size_t top;
+	size_t size;
+public:
+	StackArray() {
+		this->array = new T[80];
+		this->top = 0;
+		this->size = 80;
+	}
+	virtual ~StackArray() {
+		delete[] this->array;
+	}
+	void push(T elem) {
+		this->array[this->top++] = elem;
+	}
+	T pop() {
+		return this->array[--this->top];
+	}
 };
