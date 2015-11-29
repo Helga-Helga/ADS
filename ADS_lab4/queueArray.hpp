7ded64679e694ab1d457d717a6442e1b77c58329
helga-helga <olyalavyagina@gmail.com>
2015-11-29 16:04:35 +0200

    Implemented queue with array

--- ADS_lab4/queueArray.hpp
+++ ADS_lab4/queueArray.hpp
@@ -2,5 +2,25 @@
 #include "queue.hpp"
 
 template <class T> class QueueArray : public Queue<T> {
-
+private:
+	T *array;
+	size_t first;
+	size_t last;
+	size_t size;
+public:
+	QueueArray() {
+		this->array = new T[80];
+		this->first = 0;
+		this->last = 0;
+		this->size = 80;
+	}
+	virtual ~QueueArray() {
+		delete[] this->array;
+	}
+	void push(T elem) {
+		this->array[this->last++] = elem;
+	}
+	T pop() {
+		return this->array[this->first++];
+	}
 };
