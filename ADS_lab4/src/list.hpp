52e89b591afe3f698da5f17d5a770084544188ff
helga-helga <olyalavyagina@gmail.com>
2015-12-01 20:06:38 +0200

    Implemented Element and List

---
+++ ADS_lab4/src/list.hpp
@@ -0,0 +1,45 @@
+template <class T> class List {
+	private:
+		struct Element {
+			T value;
+			Element *next;
+			Element(T value, Element *next = NULL) {
+				this->value = value;
+				this->next = next;
+			}
+			~Element() {
+				this->next = NULL;
+			}
+		};
+		Element *first;
+		Element *last;
+	public:
+		List() {
+			this->first = this->last = NULL;
+		}
+		~List() {
+		}
+		void insert(T value, bool isLast) {
+			if (this->first == NULL && this->last == NULL) {
+				this->first = this->last = new Element(value);
+			}
+			else if (isLast) {
+				this->last = this->last->next = new Element(value);
+			}
+			else {
+				this->first->insert(value, isLast);
+			}
+		}
+		T remove() {
+			T value = this->first->value;
+			if (this->first->next == NULL) {
+				this->first = this->last = NULL;
+			}
+			else {
+				Element *tmp = this->first->next;
+				delete this->first;
+				this->first = tmp;
+			}
+			return value;
+		}
+};
