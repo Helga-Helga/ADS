c1e8261d325220c61e472ce9c57c24d40c952538
helga-helga <olyalavyagina@gmail.com>
2015-12-01 21:00:47 +0200

    Implemented tests for List

---
+++ ADS_lab4/tests/testsList.hpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <cppunit/TestCase.h>
+#include <cppunit/TestFixture.h>
+#include <cppunit/ui/text/TextTestRunner.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/TestRunner.h>
+#include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/CompilerOutputter.h>
+#include "../src/list.hpp"
+
+using namespace CppUnit;
+using namespace std;
+
+class TestList : public CppUnit::TestFixture {
+    CPPUNIT_TEST_SUITE(TestList);
+    CPPUNIT_TEST(testInsertAndRemove);
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    void setUp(void);
+    void tearDown(void);
+
+protected:
+    void testInsertAndRemove(void);
+
+private:
+    List<int> *list;
+};
+
+void TestList::setUp(void) {
+    this->list = new List<int>();
+}
+
+void TestList::tearDown(void) {
+    delete[] this->list;
+}
+
+void TestList::testInsertAndRemove(void) {
+    this->list->insert(1, 0);
+    this->list->insert(2, 0);
+    this->list->insert(3, 1);
+    CPPUNIT_ASSERT_EQUAL(2, this->list->remove());
+    CPPUNIT_ASSERT_EQUAL(1, this->list->remove());
+    CPPUNIT_ASSERT_EQUAL(3, this->list->remove());
+}
+
+CPPUNIT_TEST_SUITE_REGISTRATION( TestList );
