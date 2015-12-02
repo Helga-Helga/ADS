e5682bea3f6702c924ec4f24ffff66858d02b1dd
helga-helga <olyalavyagina@gmail.com>
2015-12-02 21:48:29 +0200

    Fixed errors -- now it works

--- ADS_lab4/src/queueArray.hpp
+++ ADS_lab4/src/queueArray.hpp
@@ -10,26 +10,32 @@ private:
 	size_t first;
 	size_t last;
 	size_t size;
+	size_t elementsCount;
 public:
 	QueueArray() {
 		this->array = new T[80];
 		this->first = 0;
 		this->last = 0;
 		this->size = 80;
+		this->elementsCount = 0;
 	}
 	virtual ~QueueArray() {
 		delete[] this->array;
 	}
 	void push(T elem) {
-		/*if (++this->last == this->first) {
-			throw overflow_error("Queue overflow");
-		}*/
+		if (this->elementsCount >= this->size) {
+			throw runtime_error("Queue overflow");
+		}
 		if (this->last == this->size) {
 			this->last = 0;
 		}
-		this->array[this->last] = elem;
+		this->array[this->last++] = elem;
+		this->elementsCount++;
 	}
 	T pop() {
+		if (this->elementsCount == 0) {
+			throw runtime_error("Queue is empty");
+		}
 		if (this->first == this->size) {
 			this->first = 0;
 		}
