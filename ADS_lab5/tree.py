6308c89be55e566ef874751a8d389f4247a947ae
helga-helga <olyalavyagina@gmail.com>
2015-12-05 21:04:06 +0200

    implemented insertion node to tree and printing tree

--- ADS_lab5/tree.py
+++ ADS_lab5/tree.py
@@ -2,8 +2,32 @@ __author__ = 'olya'
 
 
 class Node:
-    def __init__(self, data, parent, left, right):
+    def __init__(self, data=None, parent=None, left=None, right=None):
         self.data = data
         self.parent = parent
         self.left = left
         self.right = right
+
+    def insert(self, data=None):
+        if data < self.data:
+            if self.left is None:
+                self.left = Node(data, self)
+            else:
+                self.left.insert(data)
+        elif data > self.data:
+            if self.right is None:
+                self.right = Node(data, self)
+            else:
+                self.right.insert(data)
+        else:
+            self.data = data
+
+    def printTree(self):
+        if self.left:
+            self.left.printTree()
+        print self.data
+        if self.right:
+            self.right.printTree()
+
+
+
