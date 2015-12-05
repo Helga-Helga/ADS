1297892e86e52b8757866da3a545ea60f858017e
helga-helga <olyalavyagina@gmail.com>
2015-12-05 23:18:45 +0200

    Made menu

--- ADS_lab5/main.py
+++ ADS_lab5/main.py
@@ -1,8 +1,32 @@
 from tree import Node
+from file import search, delete, closeFile
 
-f = open('input.txt')
 tree = Node('')
-for line in f:
-    tree.insert(line)
-tree.printTree()
-f.close()
+with open('input.txt') as f:
+    for line in f:
+        tree.insert(line)
+print("1. Print tree\n2. Search node\n3. Delete node\n4. Exit")
+
+ans = True
+while ans:
+    ans = raw_input("What would you like to do? ")
+    if ans == "1":
+        print("Tree:")
+        tree.printTree()
+    elif ans == "2":
+        print("Searching node...")
+        data = raw_input("Enter node data: ")
+        print "Node found:", tree.search(data).data
+        print "Found in file:", search(data)
+    elif ans == "3":
+        print("Deletion node...")
+        data = raw_input("Enter node data: ")
+        tree.delete(data)
+        delete(data)
+        print("Node deleted")
+    elif ans == "4":
+        print("Goodbye")
+        closeFile()
+        ans = None
+    else:
+        print("Not valid choice. Try again")
