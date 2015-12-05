2068d5a8d5fe1e3f195fd365568b3c7f440e6f65
helga-helga <olyalavyagina@gmail.com>
2015-12-05 19:17:19 +0200

    Initial commit

---
+++ ADS_lab5/tree.py
@@ -0,0 +1,9 @@
+__author__ = 'olya'
+
+
+class Node:
+    def __init__(self, data, parent, left, right):
+        self.data = data
+        self.parent = parent
+        self.left = left
+        self.right = right
