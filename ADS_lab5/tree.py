5ee2caa1882ff6cd3105cb46a4d0e56a9c7565a3
helga-helga <olyalavyagina@gmail.com>
2015-12-05 21:49:08 +0200

    Implemented node deletion

--- ADS_lab5/tree.py
+++ ADS_lab5/tree.py
@@ -33,15 +33,63 @@ class Node:
         if data < self.data:
             if self.left is None:
                 self.left = Node(data)
-                return data
+                return self
             return self.left.search(data, self)
         elif data > self.data:
             if self.right is None:
                 self.right = Node(data)
-                return data
+                return self
             return self.right.search(data, self)
         elif data == self.data:
-            return self.data
+            return self
+
+    def delete(self, data):
+        node = self.search(data)
+        if node is not None:
+            count = node.childrenCount()
+            if count == 0:
+                if node.parent:
+                    if node.parent.left is node:
+                        node.parent.left = None
+                    else:
+                        node.parent.right = None
+                    del node
+                else:
+                    self.data = None
+            elif count == 1:
+                if node.left:
+                    n = node.left
+                else:
+                    n = node.right
+                if node.parent:
+                    if node.parent.left is node:
+                        node.parent.left = n
+                    else:
+                        node.parent.right = n
+                    del node
+                else:
+                    self.left = n.left
+                    self.right = n.right
+                    self.data = n.data
+            else:
+                parent = node
+                successor= node.right
+                while successor.left:
+                    parent = successor
+                    successor = successor.left
+                node.data = successor.data
+                if parent.left == successor:
+                    parent.left = successor.right
+                else:
+                    parent.right = successor.right
+
+    def childrenCount(self):
+        count = 0
+        if self.left:
+            count += 1
+        if self.right:
+            count += 1
+        return count
 
 
 
