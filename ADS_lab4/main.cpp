daa1ae525007f12f549a0182a5e0706894660a2c
helga-helga <olyalavyagina@gmail.com>
2015-12-05 18:33:02 +0200

    Implemented stack and queue from STL

--- ADS_lab4/main.cpp
+++ ADS_lab4/main.cpp
@@ -1,8 +1,30 @@
+#include <queue>
+#include <stack>
 #include "src/stackArray.hpp"
 #include "src/stackList.hpp"
 #include "src/queueArray.hpp"
 #include "src/queueList.hpp"
+using namespace std;
 
 int main() {
-	return 0;
+	queue<int> myQueue;
+	myQueue.push(1);
+    myQueue.push(2);
+    cout << "Queue: ";
+	while (!myQueue.empty()) {
+		cout << ' ' << myQueue.front();
+		myQueue.pop();
+	}
+	cout<<endl;
+
+    stack<int> myStack;
+    myStack.push(1);
+    myStack.push(2);
+    cout << "Stack: ";
+	while (!myStack.empty()) {
+    	cout << ' ' << myStack.top();
+    	myStack.pop();
+	   }
+	cout<<endl;
+    return 0;
 }
