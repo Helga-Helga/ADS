97e78da97d2f8eee352f489b36063d1fdab057a6
helga-helga <olyalavyagina@gmail.com>
2015-12-07 21:42:32 +0200

    Fixed search: input word in new line

--- ADS_lab5/file.py
+++ ADS_lab5/file.py
@@ -1,21 +1,22 @@
 __author__ = 'olya'
 
-datafile = open('input.txt')
+datafile = open('input.txt', "r+")
 
 
 def search(data):
     for line in datafile:
         if data in line:
             return data
+    datafile.write('\n')
     datafile.write(data)
     return data
 
 
 def delete(data):
     for line in datafile:
-        if data in line:
-            line.replace(data, '')
+        if data not in line:
+            datafile.write(data)
 
 
-def closeFile():
+def close():
     datafile.close()
